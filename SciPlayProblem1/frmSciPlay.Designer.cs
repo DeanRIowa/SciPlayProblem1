3061bc529f3d83e397d102417fec88d7d75d3bd6
DeanRIowa <DeanReedy@gmail.com>
2019-05-11 14:38:51 -0500

    Add project files.

---
+++ SciPlayProblem1/frmSciPlay.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace SciPlayProblem1
+{
+    partial class frmSciPlay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.chtMostAliveYears = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.lblMostAliveYearsDescription = new System.Windows.Forms.Label();
+            this.lblMostAliveYearsList = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chtMostAliveYears)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chtMostAliveYears
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chtMostAliveYears.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chtMostAliveYears.Legends.Add(legend1);
+            this.chtMostAliveYears.Location = new System.Drawing.Point(12, 12);
+            this.chtMostAliveYears.Name = "chtMostAliveYears";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chtMostAliveYears.Series.Add(series1);
+            this.chtMostAliveYears.Size = new System.Drawing.Size(998, 645);
+            this.chtMostAliveYears.TabIndex = 2;
+            this.chtMostAliveYears.Text = "chart2";
+            title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            title1.Name = "Title1";
+            title1.Text = "SciPlay People Alive";
+            this.chtMostAliveYears.Titles.Add(title1);
+            // 
+            // lblMostAliveYearsDescription
+            // 
+            this.lblMostAliveYearsDescription.BackColor = System.Drawing.Color.White;
+            this.lblMostAliveYearsDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMostAliveYearsDescription.Location = new System.Drawing.Point(12, 660);
+            this.lblMostAliveYearsDescription.Name = "lblMostAliveYearsDescription";
+            this.lblMostAliveYearsDescription.Size = new System.Drawing.Size(136, 79);
+            this.lblMostAliveYearsDescription.TabIndex = 3;
+            this.lblMostAliveYearsDescription.Text = "Max Alive Year(s) :";
+            // 
+            // lblMostAliveYearsList
+            // 
+            this.lblMostAliveYearsList.BackColor = System.Drawing.Color.White;
+            this.lblMostAliveYearsList.Location = new System.Drawing.Point(154, 660);
+            this.lblMostAliveYearsList.Name = "lblMostAliveYearsList";
+            this.lblMostAliveYearsList.Size = new System.Drawing.Size(855, 79);
+            this.lblMostAliveYearsList.TabIndex = 4;
+            this.lblMostAliveYearsList.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1021, 748);
+            this.Controls.Add(this.lblMostAliveYearsList);
+            this.Controls.Add(this.lblMostAliveYearsDescription);
+            this.Controls.Add(this.chtMostAliveYears);
+            this.Name = "Form1";
+            this.Text = "SciPlay";
+            ((System.ComponentModel.ISupportInitialize)(this.chtMostAliveYears)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataVisualization.Charting.Chart chtMostAliveYears;
+        private System.Windows.Forms.Label lblMostAliveYearsDescription;
+        private System.Windows.Forms.Label lblMostAliveYearsList;
+    }
+}
+
---
+++ SciPlayProblem1/frmSciPlay.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Windows.Forms.DataVisualization.Charting;
+
+
+
+namespace SciPlayProblem1
+{
+
+
+
+    public partial class frmSciPlay : Form
+    {
+        
+        public frmSciPlay()
+        {
+            InitializeComponent();
+            this.Load += new EventHandler(this.Form1_Load); //FIRES!
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SciPlayMaxYearsAlive();
+        }
+     
+     
+
+
+        public void SciPlayMaxYearsAlive()
+        {
+
+            // Set Axis titles
+            this.chtMostAliveYears.ChartAreas[0].AxisY.Title = "Occurences";
+            this.chtMostAliveYears.ChartAreas[0].AxisX.Title = "Living Years";
+         
+            // Set Chart series
+            Series series = this.chtMostAliveYears.Series[0];
+            series.Color = Color.Blue;
+            series.LegendText = "People Alive Years";
+            this.chtMostAliveYears.Series[0].YValueType = ChartValueType.Int32;
+
+            Series series2 = this.chtMostAliveYears.Series.Add("Most Alive Year(s)");
+            series2.Color = Color.Red;
+            this.chtMostAliveYears.Series[1].YValueType = ChartValueType.Int32;
+
+            // Set variables
+            int[] aliveYears = new int[101];
+            int MostAliveCount = 0;
+
+            // Load data
+            int[,] people = new int[,]
+               {
+                                                 { 1910,1926},
+                                                 { 1915,1945},
+                                                { 1924,1985},
+                                                { 1922,1985},
+                                                { 1923,1985},
+                                                { 1946,1969},
+                                                 { 1929,1937},
+                                                  { 1941,1945},
+                                                { 1956,1985}
+                                                };
+
+            // Cycle and calulcate people living per year
+            for (int i = 0; i < people.GetLength(0); i++)
+            {
+                    for (int j = people[i, 0] - 1900; j <= people[i, 1] - 1900; j++)
+                {
+                    aliveYears[j]++;
+                    if (aliveYears[j] >= MostAliveCount)
+                    {
+                        MostAliveCount = aliveYears[j];
+                    }
+                };
+            };
+
+
+            // Load chart ID most living year(s)
+            string aliveMaxYear = "";
+            for (int j = 0; j <= 100; j++)
+            {
+                if (aliveYears[j] > 0)
+                {
+                    if (aliveYears[j] == MostAliveCount)
+                    {
+                        series2.Points.AddXY(j + 1900, aliveYears[j]);
+                        if (aliveMaxYear.Length > 0)
+                        {
+                            aliveMaxYear = aliveMaxYear + ", ";
+                        };
+                        aliveMaxYear = aliveMaxYear + Convert.ToString(j + 1900);
+                    }
+                    else
+                    {
+                        series.Points.AddXY(j + 1900, aliveYears[j]);
+                    };
+                };
+            };
+
+
+            // Load Most Alive year(s)
+            this.lblMostAliveYearsList.Text =  aliveMaxYear;
+        }
+    }
+}
